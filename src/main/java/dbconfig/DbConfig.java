4c43fc693fed42941acea391ead5c4c43f85b22b
Valeria Ceron <valeria.ceron.0499@eam.edu.co>
2025-04-06 12:10:35 -0500

    Funcion creacion de tarea agregada en TareaRepository, creacion de InvalidTareaDataException y TareaValidator

--- src/main/java/dbconfig/DbConfig.java
+++ src/main/java/dbconfig/DbConfig.java
@@ -10,7 +10,7 @@ public class DbConfig {
 
     private static final String URL =  "jdbc:mysql://localhost:3306/proyectoDiseno";
     private static final String USER = "root";
-    private static final String PASSWORD = "Nico2707*";
+    private static final String PASSWORD = "ValeCM";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
---
+++ src/main/java/validator/TareaValidator.java
@@ -0,0 +1,19 @@
+
+package validator;
+
+
+public class TareaValidator {
+    
+    public static boolean validateTitulo(String titulo) {
+        return titulo != null;
+    }
+    
+    public static boolean validateDescripcion(String descripcion) {
+        return descripcion != null;
+    }
+    
+    public static boolean validatePrioridad(String prioridad) {
+        return prioridad != null;
+    }
+
+}
